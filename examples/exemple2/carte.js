bf826176d5dd2c6051bd2d9499715cd9560b1c99
Axelle Gaige <axelle.gaige@gmail.com>
2021-01-18 00:08:06 +0100

    Amélioration exemple2

--- examples/exemple2/carte.js
+++ examples/exemple2/carte.js
@@ -1,10 +1,18 @@
-var mymap;
+var mymap1;
+var mymap2;
+var mymap3;
+var mymap4;
+
+var LAMB93 = new L.Proj.CRS('IGNF:LAMB93');
+
+var utm20 = new L.Proj.CRS('EPSG:4559',
+    '+proj=utm +zone=20 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
 
 /* initMap met en place le fond de carte en spécifiant les limites de zooms possibles et,
 bien entendu, la source du fond de carte désiré, ici, OpenStreetMap. */
 function initMap(){
 
-    mymap= L.map('map',
+    mymap1 = L.map('map1',
     ).setView([50.1210604, -3.021240],3);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,7 +22,43 @@ function initMap(){
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiYWdhaWdlIiwiYSI6ImNrZ2huODE5YjBnMGoyc3RldGZkZThxcHgifQ.Hwxwc-MYlZ01QTPAIFSloQ'
-  }).addTo(mymap);
+  }).addTo(mymap1);
+
+  mymap2 = L.map('map2',{
+      crs:L.geoportalCRS.EPSG2154
+  }
+    ).setView([50.1210604, -3.021240],3);
+    
+    var lyr = L.geoportalLayer.WMTS({
+        layer: "GEOGRAPHICALGRIDSYSTEMS.MAPS",
+    }, { // leafletParams
+        opacity: 0.8
+    });
+    lyr.addTo(mymap2); // ou map.addLayer(lyr);
+
+  mymap3 = L.map('map3',
+    ).setView([50.1210604, -3.021240],3);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      minZoom:1,
+      maxZoom: 18,
+      id: 'mapbox/satellite-v9',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiYWdhaWdlIiwiYSI6ImNrZ2huODE5YjBnMGoyc3RldGZkZThxcHgifQ.Hwxwc-MYlZ01QTPAIFSloQ'
+  }).addTo(mymap3);
+
+  mymap4 = L.map('map4',
+    ).setView([50.1210604, -3.021240],3);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      minZoom:1,
+      maxZoom: 18,
+      id: 'mapbox/satellite-v9',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiYWdhaWdlIiwiYSI6ImNrZ2huODE5YjBnMGoyc3RldGZkZThxcHgifQ.Hwxwc-MYlZ01QTPAIFSloQ'
+  }).addTo(mymap4);
 }
 
 initMap();
@@ -23,7 +67,11 @@ initMap();
 
 
 //Affiche le systeme de coordonnées de référence
-document.getElementById('scr').innerHTML = mymap.options.crs.code;
+document.getElementById('scr1').innerHTML = mymap1.options.crs.code;
+document.getElementById('scr2').innerHTML = mymap2.options.crs.code;
+document.getElementById('scr3').innerHTML = mymap3.options.crs.code;
+document.getElementById('scr4').innerHTML = mymap4.options.crs.code;
+
 
 
 
@@ -104,5 +152,11 @@ function majRectangle_hg(event, coin_hg, coin_bg, coin_hd,coin_bd, rectangle){
 
 
 
-var hello = rectangleInt([[51.1564,-5.1084],[41.1842,10.0195]]);
-hello.addTo(mymap);
+var hello1 = rectangleInt([[51.1564,-5.1084],[41.1842,10.0195]]);
+var hello2 = rectangleInt([[51.1564,-5.1084],[41.1842,10.0195]]);
+var hello3 = rectangleInt([[51.1564,-5.1084],[41.1842,10.0195]]);
+var hello4 = rectangleInt([[51.1564,-5.1084],[41.1842,10.0195]]);
+hello1.addTo(mymap1);
+hello2.addTo(mymap2);
+hello3.addTo(mymap3);
+hello4.addTo(mymap4);
--- examples/exemple2/index.php
+++ examples/exemple2/index.php
@@ -11,20 +11,37 @@
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
     integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
     crossorigin=""></script>
-    <script src="https://cdn.jsdelivr.net/npm/leaflet.geodesic"
-    integrity="sha512-5hu8xzx09aoq4eHRCNLXRAmwYNgsMzBSLj02kPAaD48G03otgNf7PIOJaHlchkK2kfTAMhO3oaTxX5HgniXb2g=="
-    crossorigin=""></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.6.3/proj4.js"></script>
-    <script src="../../lib/proj4leaflet.js"></script>
+
+    <script data-key="choisirgeoportail" src="../../lib/GpPluginLeaflet-2.1.7/GpPluginLeaflet.js"></script>
+    <link data-key="choisirgeoportail" rel="stylesheet" href="../../lib/GpPluginLeaflet-2.1.7/GpPluginLeaflet.css" />
     
   </head>
 
   <body>
-      <div id="titre">
-        <h1>Rectangle géodésique intéractif</h1>
-        Systeme de coordonnées de référence : <span id = "scr"></span>
-      </div>
-    <div id = map></div>
+    <div id="titre">
+      <h1>Rectangle géodésique intéractif</h1>
+    </div>
+
+    <div id = carte1>
+      Systeme de coordonnées de référence : <span id = "scr1"></span>
+      <div id = map1></div>
+    </div>
+
+    <div id = carte2>
+      Systeme de coordonnées de référence : <span id = "scr2"></span>
+      <div id = map2></div>
+    </div>
+    
+    <div id = carte3>
+      Systeme de coordonnées de référence : <span id = "scr3"></span>
+      <div id = map3></div>
+    </div>
+    
+    <div id = carte4>
+      Systeme de coordonnées de référence : <span id = "scr4"></span>
+      <div id = map4></div>
+    </div>
+    
     <script src="../../src/rectangleGeo.js"></script>
     <script type="text/javascript" src="carte.js"></script>
   </body>
